d89e6c7180dde9d4b0b61f4a5373b9d1d08381f7
Mehul Mohan <mehulmpt@gmail.com>
2021-05-21 01:04:15 +0530

    Test run: Deploy to EC2 workflow

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -7,7 +7,6 @@ set -e
 mkdir ~/.ssh
 touch ~/.ssh/id_rsa
 echo $PRIVATE_SSH_KEY | base64 -d > ~/.ssh/id_rsa
-echo "" >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # copy package files inside build folder
