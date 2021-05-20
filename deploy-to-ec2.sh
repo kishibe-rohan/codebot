7f80d938bd92e22fd0e039dd89e5a4eec784a2a5
Mehul Mohan <mehulmpt@gmail.com>
2021-05-21 01:16:23 +0530

    try now

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -13,6 +13,12 @@ chmod 600 ~/.ssh/id_rsa
 # copy package files inside build folder
 cp package.json yarn.lock build
 
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -r build ubuntu@$EC2_IP:~/
+scp -o StrictHostKeyChecking=no -r build ubuntu@$EC2_IP:~/
+
+ssh ubuntu@$EC2_IP <<'EOL'
+	cd ~/build
+    yarn install --frozen-lockfile
+    yarn start:prod
+EOL
 
 echo "Done"
