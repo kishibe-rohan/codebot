8deea02e7800ddc6652f55b7733d886244446286
Mehul Mohan <mehulmpt@gmail.com>
2021-05-22 00:41:50 +0530

    Deploy script fixes

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -11,6 +11,7 @@ echo "" >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # copy env
+touch ~/build/.env.sh
 echo "export BOT_TOKEN=$BOT_TOKEN" >> ~/build/.env.sh
 echo "export SHARED_SECRET=$SHARED_SECRET" >> ~/build/.env.sh
 chmod 0755 ~/build/.env.sh
