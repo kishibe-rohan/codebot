f1854dd813f5887bd236309dceccd5389ee12933
Mehul Mohan <mehulmpt@gmail.com>
2021-05-22 00:43:50 +0530

    Deploy script fixes

--- deploy-to-ec2.sh
+++ deploy-to-ec2.sh
@@ -11,10 +11,10 @@ echo "" >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # copy env
-touch ~/build/.env.sh
-echo "export BOT_TOKEN=$BOT_TOKEN" >> ~/build/.env.sh
-echo "export SHARED_SECRET=$SHARED_SECRET" >> ~/build/.env.sh
-chmod 0755 ~/build/.env.sh
+touch build/.env.sh
+echo "export BOT_TOKEN=$BOT_TOKEN" >> build/.env.sh
+echo "export SHARED_SECRET=$SHARED_SECRET" >> build/.env.sh
+chmod 0755 build/.env.sh
 
 # copy package files inside build folder
 cp package.json yarn.lock build
